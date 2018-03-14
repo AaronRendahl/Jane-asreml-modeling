be2afc4aaf02d806d987c4ddba268a833437b61d
Aaron Rendahl <rend0020@umn.edu>
2018-03-14 08:22:28 -0500

    need to add additional parameters to get convergence

--- asreml_msi_modeling.R
+++ asreml_msi_modeling.R
@@ -4,7 +4,7 @@ OST_R_asreml_NS_hr <- read.csv("OST_R_asreml_NS_hr.csv", check.names=FALSE)
 OST_R_asreml_NS_hr$Number_ID <- factor(OST_R_asreml_NS_hr$Number_ID)
 
 m1 <- asreml(glucose ~ time + time2, data=OST_R_asreml_NS_hr, random=~str(~Number_ID + time:Number_ID + time2:Number_ID, ~us(3):id(Number_ID)) + ~Owner, rcov=~units, maxit=400,  Cfixed=TRUE)
-m1 <- asreml(glucose ~ time + time2, data=OST_R_asreml_NS_hr, random=~str(~Number_ID + time:Number_ID + time2:Number_ID, ~us(3):id(Number_ID)))
+m1 <- asreml(glucose ~ time + time2, data=OST_R_asreml_NS_hr, random=~str(~Number_ID + time:Number_ID + time2:Number_ID, ~us(3):id(Number_ID)), rcov=~units, maxit=400,  Cfixed=TRUE)
 
 ## previous output
 ## glucose works with farm random intercept and breed fixed effect
